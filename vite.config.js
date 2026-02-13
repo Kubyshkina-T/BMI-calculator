9b3d81dac9f43a8e029abf64416f47c1b5aaefa1
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-02-13 19:22:33 +0200

    json add

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/BMI-calculator/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        under: "page-under.html",
+        norm: "page-norm.html",
+        over: "page-over.html",
+      },
+    },
+  },
 });
